a14db1b5fb916f35935b74f63e345f2613ddebc1
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-09-30 19:50:55 +0600

    Porject core setup completed

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,32 @@
-import express, { Application, Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app: Application = express();
 
-// Parsers
-app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:3000"],
     credentials: true,
   })
 );
+app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
+// Parsers
+app.use(express.json());
+
+// app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("App is listening on port 5000");
 });
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
